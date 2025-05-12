7aa3fef3ef97e8ee94cf0b96cd9918838936c4d0
Gabrielle Puno Lara <165493895+glaraterpmail@users.noreply.github.com>
2025-05-12 12:12:01 -0400

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  root: 'vite', // Set root directory based on project structure
+  plugins: [vue()],
+  server: {
+    port: 3001, // Maintain port configuration
+    strictPort: true, // Prevent automatic port switching
+    host: '127.0.0.1'
+  },
+  build: {
+    outDir: 'vite/build', 
+    emptyOutDir: true, // Clears previous builds
+    rollupOptions: {
+      input: 'vite/INST377 - MET Home.html' 
+    }
+  }
+});
