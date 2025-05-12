855222b652e50d813a6207ec11b0db27b963fc96
Gabrielle Puno Lara <165493895+glaraterpmail@users.noreply.github.com>
2025-05-12 11:21:02 -0400

    Update vite.config.js

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
-export default {
+export default defineConfig({
+  root: 'src', // Set the root directory to match your project structure
+  plugins: [vue()],
   server: {
     port: 3001, // Change to the new port
     strictPort: true, // Prevent automatic port switching
     host: '127.0.0.1'
+  },
+  build: {
+    outDir: '../dist', // Ensures build output is outside "src"
+    emptyOutDir: true, // Clears previous builds
   }
-};
+});
